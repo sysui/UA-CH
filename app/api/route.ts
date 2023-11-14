9ffead12067d7ce0e025e1587a94af162df580e6
sysui <99@eeu.jp>
2023-11-14 00:14:32 +0000

    Update response format in GET function.

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,5 +3,5 @@ import { UAParser } from "ua-parser-js";
 export async function GET(request: Request) {
   // @ts-ignore ua-parser-js@2.0のd.tsがないからエラーを無視
   const ua = UAParser(Object.fromEntries(request.headers)).withClientHints();
-  return Response.json(ua);
+  return new Response(JSON.stringify(ua, null, 2));
 }
