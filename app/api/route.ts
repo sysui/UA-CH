c69f3dce8ede38c6d8c0e962f8cbc8c627959a9c
sysui <84460440+sysui@users.noreply.github.com>
2025-01-13 04:43:54 +0000

    fix: remove ignore comment

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,6 @@
 import { UAParser } from "ua-parser-js";
 
 export async function GET(request: Request) {
-  // @ts-ignore ua-parser-js@2.0のd.tsがないからエラーを無視
   const ua = UAParser(Object.fromEntries(request.headers)).withClientHints();
   return new Response(JSON.stringify(ua, null, 2));
 }
