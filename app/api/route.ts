48cb9303cb9f351e801b67e4360aa7bd8c9e7542
sysui <84460440+sysui@users.noreply.github.com>
2025-04-10 14:41:36 +0000

    fix: add Content-Type header to JSON response in GET handler

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,5 +2,9 @@ import { UAParser } from "ua-parser-js";
 
 export async function GET(request: Request) {
   const ua = UAParser(Object.fromEntries(request.headers)).withClientHints();
-  return new Response(JSON.stringify(ua, null, 2));
+  return new Response(JSON.stringify(ua, null, 2), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
