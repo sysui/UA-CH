27e57bf2c65d868f6487aa22b6affc9564e5f7be
sysui <84460440+sysui@users.noreply.github.com>
2025-06-25 16:39:14 +0000

    fix: update api routing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [UAParser.js](https://uaparser.dev/)を使ってUser-Agent Client Hints (UA-CH)を取得するデモ
 
-https://uach.vercel.app/api
+https://uach.vercel.app/
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+export const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/api",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
